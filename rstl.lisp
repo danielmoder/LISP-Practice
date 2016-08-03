83c11947bcfb1d00c2499c25237decf2fcd82c6b
danielmoder <daniel.j.moder.18@dartmouth.edu>
2016-08-02 20:18:01 -0400

    Update rstl.lisp

--- rstl.lisp
+++ rstl.lisp
@@ -2,10 +2,8 @@
 ; This program converts from Ruby S-expressions to LISP for any arithmetic expression
 
 ; Sample argument:
-;(:program ((:binary (:binary (:@int "4" (1 0)) :/ (:@int "2" (1 2))) :+ (:binary (:@int "9" (1 4)) :/ (:@int "3" (1 6))))))
-; `--> (Ruby S-expression for "4/2+9/3")
-
-
+; (:program ((:binary (:binary (:@int "4" (1 0)) :/ (:@int "2" (1 2))) :+ (:binary (:@int "9" (1 4)) :/ (:@int "3" (1 6))))))
+;  `--> (Ruby S-expression for "4/2+9/3")
 
 
 ; Format operations appropriately for LISP
@@ -16,33 +14,23 @@
 	((eql op :/) '/)))
 
 
-
 ; Takes Ruby S-expression of an arithmetic expression
 ; Returns the same arithmetic expression in LISP syntax
 
 (defun ruby-sexp-to-lisp (exp)
-  
-
-  (cond ((null exp) null)
-	((atom exp) exp)
-	; If empty list or single item:
-	; return the same
-	(t
-	; Else:
-	 (let ((tag (car exp))
-	       (args (cdr exp)))
-	       ; prefixed tag describes operation (
-	       ; car = (tag)
-	       ; cdr = (args)
+  (cond ((null exp) null)						; If empty, return empty
+	((atom exp) exp)						; If atom, return it
+	(t								; Else:
+	
+	 (let ((tag (car exp))						; prefixed tag describes operation
+	       (args (cdr exp)))					; car = tag, cdr = args
+	       
 	   (cond ((eql tag :program) (ruby-sexp-to-lisp (caar args)))
-		 ; ":program" appears at beginning --> return the remainder of the S-exp.
-
+	   	 ; ":program" appears at beginning --> return the remainder of the S-exp.
 		 ((eql tag :@int) (parse-integer (nth 0 args)))
-		 ; ":@int" takes only a single integer argument
-		 ; these are leaf nodes --> return the integer
-
+		 ; ":@int" takes only a single integer argument: these are leaf nodes --> return the integer
 		 ((eql tag :binary)
-		  ; ":binary" operators! this is the good stuff
+		 ; ":binary" operators! this is the good stuff
 
 		  (let ((op (nth 1 args))
 			(arg1 (nth 0 args))
@@ -52,4 +40,3 @@
 		    (list (op-clean op) (ruby-sexp-to-lisp arg1) (ruby-sexp-to-lisp arg2)))))))))
                     ; and reorder for LISP!
                     ; (...and recurse)
-
